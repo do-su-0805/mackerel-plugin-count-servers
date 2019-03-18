90c303f1048deb811e18407bb47bb5e70c5672fe
do-su-0805 <do-su-0805@hatena.ne.jp>
2019-03-18 20:45:37 +0900

    delete sample

--- mackerel-plugin-count-servers.sh
+++ mackerel-plugin-count-servers.sh
@@ -37,20 +37,6 @@ mkr hosts -s ${service} > $TMP_RESULT
 for role in $(mkr services | jq ".[] | select(.name == \"${service}\") | .roles[]" -r); do
   for status in working standby maintenance poweroff; do
     count=`cat $TMP_RESULT | jq "[.[] | select(.roleFullnames[] == \"${service}:${role}\") | select(.status == \"${status}\") | .id] | length"`;
-#  sample_name=`mkr hosts --service ${service} -role ${role} | jq '.[0].name' -r`;
-#  echo "<tr><td>${service}:${role}</td><td>${count}</td><td>${sample_name}</td></tr>";
     echo -e "${service}.${role}.${status}\t${count}\t$(date -u +%s)"
   done
-done 
-#echo "</table>"
-
-
-#mkr hosts -v | jq '.[] | {"platform" : (.meta | .kernel | .platform_name), "version": (.meta | .kernel | .platform_version)}' -c > $RESULT_FILE
-
-#for platform in $(cat $RESULT_FILE | jq .platform -r | sort | uniq | awk '!/null/{print $1}'); do
-#  for version in $(cat $RESULT_FILE | grep $platform | jq .version -r | sort | uniq ); do
-#    count=`cat $RESULT_FILE | grep ${platform} | jq .version -r | grep -E "^${version}$" | wc -l`
-#    display_version=`echo $version | sed -e 's/\./-/g'`
-#    echo -e "os.${platform}.${display_version}\t${count}\t$(date -u +%s)"
-#  done
-#done
+done
